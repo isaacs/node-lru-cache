345d9c7275c2a373000d3eeff1e566f752154055
Trent Mick <trentm@gmail.com>
2012-03-27 13:26:04 -0700

    <cache>.dump() for debugging/observability, no guarantees out response structure

--- test/basic.js
+++ test/basic.js
@@ -60,7 +60,7 @@ test('maxLength', function (t) {
   for (var i = 98; i < 100; i ++) {
     t.equal(cache.get(i), i)
   }
-  
+
   // now remove the maxLength restriction, and try again.
   cache.maxLength = "hello"
   for (var i = 0; i < 100; i ++) cache.set(i, i)
@@ -89,5 +89,29 @@ test('reset', function (t) {
   t.equal(cache.maxLength, 10)
   t.equal(cache.get("a"), undefined)
   t.equal(cache.get("b"), undefined)
+  t.end()
+})
+
+// Note: `<cache>.dump()` is a debugging tool only. No guarantees are made
+// about the format/layout of the response.
+test('dump', function (t) {
+  var cache = new LRU(10)
+  var d = cache.dump();
+  t.equal(Object.keys(d).length, 0, "nothing in dump for empty cache")
+  cache.set("a", "A")
+  var d = cache.dump()  // { a: { key: 'a', value: 'A', lu: 0 } }
+  t.ok(d.a)
+  t.equal(d.a.key, 'a')
+  t.equal(d.a.value, 'A')
+  t.equal(d.a.lu, 0)
+
+  cache.set("b", "B")
+  cache.get("b")
+  d = cache.dump()
+  t.ok(d.b)
+  t.equal(d.b.key, 'b')
+  t.equal(d.b.value, 'B')
+  t.equal(d.b.lu, 2)
+
   t.end()
 })
