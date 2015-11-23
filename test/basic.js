abaf636528d4dcf5a760274121dc50ac673b7480
Ted Pennings <tpennings@newrelic.com>
2015-11-23 00:09:29 -0800

    Only accept strings and numbers as keys (#53)

--- test/basic.js
+++ test/basic.js
@@ -229,7 +229,7 @@ test("drop the old items", function(t) {
   }, 155)
 })
 
-test("individual item can have it's own maxAge", function(t) {
+test("individual item can have its own maxAge", function(t) {
   var cache = new LRU({
     max: 5,
     maxAge: 50
@@ -242,7 +242,7 @@ test("individual item can have it's own maxAge", function(t) {
   }, 25)
 })
 
-test("individual item can have it's own maxAge > cache's", function(t) {
+test("individual item can have its own maxAge > cache's", function(t) {
   var cache = new LRU({
     max: 5,
     maxAge: 20
@@ -391,6 +391,95 @@ test("pop the least used item", function (t) {
   t.equal(last, null)
   t.equal(cache.length, 0)
   t.equal(cache.max, 3)
+
+  t.end()
+})
+
+test("get and set only accepts strings and numbers as keys", function(t) {
+  var cache = new LRU()
+
+  cache.set("key", "value")
+  cache.set(123, 456)
+
+  t.equal(cache.get("key"), "value")
+  t.equal(cache.get(123), 456)
+
+  t.throws(function() {
+    cache.set({ someObjectKey: true }, "a")
+  }, "set should not accept objects as keys")
+
+  t.throws(function() {
+    cache.get({ someObjectKey: true })
+  }, "get should not accept objects as keys")
+
+  t.throws(function() {
+    cache.set([1,2,3], "b")
+  }, "set should not accept arrays as keys")
+
+  t.throws(function() {
+    cache.get([1,2,3])
+  }, "get should not accept arrays as keys")
+
+  t.end()
+})
+
+test("peek only accepts strings and numbers as keys", function(t) {
+  var cache = new LRU()
+
+  cache.set("key", "value")
+  cache.set(123, 456)
+
+  t.equal(cache.peek("key"), "value")
+  t.equal(cache.peek(123), 456)
+
+  t.throws(function() {
+    cache.peek({ someObjectKey: true })
+  }, "peek should not accept objects as keys")
+
+  t.throws(function() {
+    cache.peek([1,2,3])
+  }, "peek should not accept arrays as keys")
+
+  t.end()
+})
+
+test("del only accepts strings and numbers as keys", function(t) {
+  var cache = new LRU()
+
+  cache.set("key", "value")
+  cache.set(123, 456)
+
+  cache.del("key")
+  cache.del(123)
+
+  t.assertNot(cache.has("key"))
+  t.assertNot(cache.has(123))
+
+  t.throws(function() {
+    cache.del({ someObjectKey: true })
+  }, "del should not accept objects as keys")
+
+  t.throws(function() {
+    cache.del([1,2,3])
+  }, "del should not accept arrays as keys")
+
+  t.end()
+})
+
+
+test("has only accepts strings and numbers as keys", function(t) {
+  var cache = new LRU()
+
+  cache.has("key")
+  cache.has(123)
+
+  t.throws(function() {
+    cache.has({ someObjectKey: true })
+  }, "has should not accept objects as keys")
+
+  t.throws(function() {
+    cache.has([1,2,3])
+  }, "has should not accept arrays as keys")
 
   t.end()
 })
