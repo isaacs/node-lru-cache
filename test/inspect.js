1c48788836e63320ff0355263509cd2a72a30106
Chris Cinelli <chris@allestelle.com>
2018-11-20 12:31:14 -0800

    Fixing and simplifying test-inspect.js using snapshot testing (see https://www.node-tap.org/snapshots/ )

--- test/inspect.js
+++ test/inspect.js
@@ -5,50 +5,54 @@ var LRU = require('../')
 
 var l = LRU()
 
-function inspect (str) {
-  t.equal(util.inspect(l), str)
-  t.equal(l.inspect(), str)
+function cleanTime (output) {
+  return output.replace(/now:\s*[0-9]+,/g, 'now: {time},')
 }
 
-inspect('LRUCache {}')
+function inspect () {
+  t.matchSnapshot(cleanTime(util.inspect(l)), 'output')
+  t.matchSnapshot(cleanTime(l.inspect()), 'output')
+}
+
+inspect()
 
 l.max = 10
-inspect('LRUCache {\n  max: 10\n}')
+inspect()
 
 l.maxAge = 50
-inspect('LRUCache {\n  max: 10,\n  maxAge: 50\n}')
+inspect()
 
 l.set({ foo: 'bar' }, 'baz')
-inspect("LRUCache {\n  max: 10,\n  maxAge: 50,\n\n  { foo: 'bar' } => { value: 'baz' }\n}")
+inspect()
 
 l.maxAge = 0
 l.set(1, {a: {b: {c: {d: {e: {f: {}}}}}}})
-inspect("LRUCache {\n  max: 10,\n\n  1 => { value: { a: { b: [Object] } } },\n  { foo: 'bar' } => { value: 'baz', maxAge: 50 }\n}")
+inspect()
 
 l.allowStale = true
-inspect("LRUCache {\n  allowStale: true,\n  max: 10,\n\n  1 => { value: { a: { b: [Object] } } },\n  { foo: 'bar' } => { value: 'baz', maxAge: 50 }\n}")
+inspect()
 
 setTimeout(function () {
-  inspect("LRUCache {\n  allowStale: true,\n  max: 10,\n\n  1 => { value: { a: { b: [Object] } } },\n  { foo: 'bar' } => { value: 'baz', maxAge: 50, stale: true }\n}")
+  inspect()
 
   // prune stale items
   l.forEach(function () {})
-  inspect('LRUCache {\n  allowStale: true,\n  max: 10,\n\n  1 => { value: { a: { b: [Object] } } }\n}')
+  inspect()
 
   l.lengthCalculator = function () { return 5 }
-  inspect('LRUCache {\n  allowStale: true,\n  max: 10,\n  length: 5,\n\n  1 => { value: { a: { b: [Object] } }, length: 5 }\n}')
+  inspect()
 
   l.max = 0
-  inspect('LRUCache {\n  allowStale: true,\n  length: 5,\n\n  1 => { value: { a: { b: [Object] } }, length: 5 }\n}')
+  inspect()
 
   l.maxAge = 100
-  inspect('LRUCache {\n  allowStale: true,\n  maxAge: 100,\n  length: 5,\n\n  1 => { value: { a: { b: [Object] } }, maxAge: 0, length: 5 }\n}')
+  inspect()
   l.allowStale = false
-  inspect('LRUCache {\n  maxAge: 100,\n  length: 5,\n\n  1 => { value: { a: { b: [Object] } }, maxAge: 0, length: 5 }\n}')
+  inspect()
 
   l.maxAge = 0
-  inspect('LRUCache {\n  length: 5,\n\n  1 => { value: { a: { b: [Object] } }, length: 5 }\n}')
+  inspect()
 
   l.lengthCalculator = null
-  inspect('LRUCache {\n  1 => { value: { a: { b: [Object] } } }\n}')
+  inspect()
 }, 100)
