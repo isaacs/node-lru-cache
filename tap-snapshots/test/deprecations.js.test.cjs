de3ce6385b536e7eca4c849f79ba52b5be3a02de
isaacs <i@izs.me>
2022-01-21 10:25:13 -0800

    add missing snapshots

---
+++ tap-snapshots/test/deprecations.js.test.cjs
@@ -0,0 +1,35 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/deprecations.js TAP > must match snapshot 1`] = `
+Array [
+  Array [
+    "please use options.allowStale instead",
+    "DeprecationWarning",
+    "LRU_CACHE_stale",
+    Function LRUCache(classLRUCache),
+  ],
+  Array [
+    "please use options.ttl instead",
+    "DeprecationWarning",
+    "LRU_CACHE_maxAge",
+    Function LRUCache(classLRUCache),
+  ],
+  Array [
+    "please use options.sizeCalculation instead",
+    "DeprecationWarning",
+    "LRU_CACHE_length",
+    Function LRUCache(classLRUCache),
+  ],
+  Array [
+    "please use cache.size instead",
+    "DeprecationWarning",
+    "LRU_CACHE_length_getter",
+    Function get length(),
+  ],
+]
+`
