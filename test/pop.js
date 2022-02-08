38dde3fad499b66286cb2269b4a111ba5e8bd979
isaacs <i@izs.me>
2022-02-08 11:58:55 -0800

    test for pop()

---
+++ test/pop.js
@@ -0,0 +1,14 @@
+const t = require('tap')
+const LRU = require('../')
+const cache = new LRU({ max: 5 })
+for (let i = 0; i < 5; i++) {
+  cache.set(i, i)
+}
+cache.get(2)
+const popped = []
+let p
+do {
+  p = cache.pop()
+  popped.push(p)
+} while (p !== undefined)
+t.same(popped, [0, 1, 3, 4, 2, undefined])
