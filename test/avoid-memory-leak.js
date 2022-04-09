59705003ac36ddd0d844991fe764a60bf8c50a26
isaacs <i@izs.me>
2022-04-09 12:03:31 -0700

    Fix memory leak in unbounded storage case
    Evictions were no being tracked properly when maxSize triggered an
    eviction.
    
    In the max-bound storage case, this wasn't so much of a problem, because
    the storage couldn't grow unbounded anyway.  However, since size-evicted
    indexes were not being added to the `free` list, it would effectively
    reduce the available storage space, holding onto old entries and
    shortening the available storage area, which would result in fewer items
    being cached (except some that were cached indefinitely!)
    
    In the unbounded case, this was much worse.  Because the storage array
    was unbounded, the cache effectively always remained in the initial
    "fill to completion" state, resulting in holding onto _every_ item
    forever, while thinking that the `calculatedSize` was being limited to
    `maxSize`!  Pretty bad.
    
    (This will be back-ported to other 7.x versions as a patch.)
    
    Thanks to @moish83 for the report.
    
    Fix: https://github.com/isaacs/node-lru-cache/issues/227

---
+++ test/avoid-memory-leak.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node --expose-gc
+
+// https://github.com/isaacs/node-lru-cache/issues/227
+
+const t = require('tap')
+
+const maxSize = 100_000
+const itemSize = 1_000
+const profEvery = 10_000
+const n = 1_000_000
+
+if (typeof gc !== 'function') {
+  t.plan(0, 'run with --expose-gc')
+  process.exit(0)
+}
+
+const tryReq = mod => {
+  try {
+    return require(mod)
+  } catch (er) {
+    t.plan(0, `need ${mod} module`)
+    process.exit(0)
+  }
+}
+
+const v8 = tryReq('v8')
+
+const LRUCache = require('../')
+const expectItemCount = Math.ceil(maxSize / itemSize)
+const max = expectItemCount + 1
+const keyRange = expectItemCount * 2
+
+// fine to alloc unsafe, we don't ever look at the data
+const makeItem = () => Buffer.allocUnsafe(itemSize)
+
+const prof = (i, cache) => {
+  // run gc so that we know if we're actually leaking memory, or just
+  // that the gc is being lazy and not responding until there's memory
+  // pressure.
+  gc()
+  return {
+    i,
+    ...v8.getHeapStatistics(),
+    valListLength: cache.valList.length,
+    freeLength: cache.free.length,
+  }
+}
+
+const runTest = async (t, cache) => {
+  // first, fill to expected size
+  for (let i = 0; i < expectItemCount; i++) {
+    cache.set(i, makeItem())
+  }
+
+  // now start the setting and profiling
+  const profiles = []
+  for (let i = 0; i < n; i++) {
+    if ((i % profEvery) === 0) {
+      const profile = prof(i, cache)
+      t.ok(
+        profile.valListLength <= max,
+        `expect valList to have fewer than ${max} items`,
+        { found: profile.valListLength },
+      )
+      t.ok(
+        profile.freeLength <= 1,
+        'expect free stack to have <= 1 item',
+        { found: profile.freeLength },
+      )
+      t.equal(profile.number_of_native_contexts, 1, '1 native context')
+      t.equal(profile.number_of_detached_contexts, 0, '0 native context')
+      profiles.push(profile)
+    }
+
+    const item = makeItem()
+    cache.set(i % keyRange, item)
+  }
+
+  const profile = prof(n, cache)
+  profiles.push(profile)
+
+  // Warning: kludgey inexact test!
+  // memory leaks can be hard to catch deterministically.
+  // The first few items will tend to be lower, and we'll see
+  // *some* modest increase in heap usage from tap itself as it
+  // runs the test and builds up its internal results data.
+  // But, after the initial few profiles, it should be modest.
+  // Considering that the reported bug showed a 10x increase in
+  // memory in this reproduction case, 2x is still pretty aggressive,
+  // without risking false hits from other node or tap stuff.
+
+  const start = Math.floor(profiles.length / 2)
+  const initial = profiles[start]
+  for (let i = start; i < profiles.length; i++) {
+    const current = profiles[i]
+    const delta = current.total_heap_size / initial.total_heap_size
+    t.ok(delta < 2, 'memory growth should not be unbounded', {
+      delta,
+      current,
+      initial,
+    })
+  }
+}
+
+t.test('both max and maxSize', t =>
+  runTest(t, new LRUCache({
+    maxSize,
+    sizeCalculation: s => s.length,
+    max,
+  })))
+
+t.test('only max, no maxSize', t =>
+  runTest(t, new LRUCache({ max })))
