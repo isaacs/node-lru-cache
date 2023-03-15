f3c8e812897151bf18c856e60ee180cad995aa32
isaacs <i@izs.me>
2023-03-15 11:41:38 -0700

    include sourcemaps for minified versions

--- fixup.sh
+++ fixup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 esbuild --minify \
+  --sourcemap \
   --bundle dist/cjs/index-cjs.js \
   --outfile=dist/cjs/index.min.js \
   --format=cjs
 
 esbuild --minify \
+  --sourcemap \
   --bundle dist/mjs/index.js \
   --outfile=dist/mjs/index.min.js \
   --format=esm
